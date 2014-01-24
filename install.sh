28822cc8a76f6afa95ba4a8407965381d4e00a53
xqx <xqx@klee.(none)>
2014-01-24 19:13:09 +0800

    add some plugins : grep in vim shell in vim ..
    Signed-off-by: xqx <xqx@klee.(none)>

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-cp ~/.vim ~/.vim_bak
+cp -r ~/.vim ~/.vim_bak
 
 cp ~/.vimrc ~/.vimrc_bak
 
 rm -rf ~/.vim ~/.vimrc
 
-cp vimfiles  ~/.vim 
+cp -r vimfiles  ~/.vim 
 
 cp _vimrc  ~/.vimrc 
